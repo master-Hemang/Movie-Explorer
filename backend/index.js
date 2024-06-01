2693d22ad4d04d84b97391d325103e71a3b6a7cf
Hemang Vijay Borse <101041055+master-Hemang@users.noreply.github.com>
2024-06-01 18:33:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,12 @@ const moviesRouter = require('./routes/movies');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const corsOptions = {
+  origin: 'https://movie-explorer-gamma.vercel.app/',
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 
 app.use(cors());
 app.use(express.json());
